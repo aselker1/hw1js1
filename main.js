43074f06340ea180f5c1f554e4a99d8a04967885
aselker1 <mamatovaaselya28@gmail.com>
2023-08-29 21:08:45 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+//определите число четное или нечетное
+
+
+let num ='11';
+
+if(num % 2 ==0){
+    console.log('четное');
+} else if(num % 3 ==0  ){
+    console.log('нечетное');
+} else if(num % 2 ==1){
+    console.log('нечетное');
+} else{
+    console.log('введите только цифры');
+}
+
+
+
